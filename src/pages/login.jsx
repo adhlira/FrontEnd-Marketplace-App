d897c012c6e7ccc6c82a633d698ad651ab640ab5
adhlira <adhliramadhan68@gmail.com>
2024-12-02 16:28:26 +0700

    fix error handling login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Link, useNavigate } from "react-router-dom";
+import { data, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
@@ -8,63 +8,73 @@ const login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState({ email: "", password: "" });
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await axios.post("http://localhost:3000/login", {
-        email,
-        password,
-      });
-      console.log(response);
+    const errors = {};
 
-      if (response.status === 200) {
-        // Simpan data user di localStorage
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-        alert("Succesfully Login!");
-        navigate("/index-seller");
-      }
-    } catch (error) {
-      if (error.response && error.response.status === 400) {
-        setErrorMessage(error.response.data.message);
-        console.log("Ini adalah error message : ", errorMessage);
-      }
-      // setEmail("");
-      // setPassword("");
+    if (!email) {
+      errors.email = "Email tidak boleh kosong";
     }
+    if (!password) {
+      errors.password = "Password tidak boleh kosong";
+    }
+    setErrorMessage(errors);
+
+    if (Object.keys(errors).length === 0)
+      try {
+        const response = await axios.post("http://localhost:3000/login", {
+          email,
+          password,
+        });
+        console.log("Ini adalah response", response);
+
+        if (response.status === 200) {
+          // Simpan data user di localStorage
+          localStorage.setItem("user", JSON.stringify(response.data.user));
+          alert("Succesfully Login!");
+          navigate("/index-seller");
+        }
+      } catch (error) {
+        console.log("ini adalah error", error);
+        if (error.response && error.response.status === 400) {
+          alert("Email atau password salah");
+          setEmail("");
+          setPassword("");
+        }
+      }
   };
 
   return (
     <>
       <div className="bg-blue-800 flex justify-center h-screen">
-        <div className="container mx-auto place-self-center rounded-md p-5 w-80 h-96 bg-gray-800 font-serif">
+        <div className="container mx-auto place-self-center rounded-md p-5 w-80 h-[420px] bg-gray-800 font-serif">
           <h1 className="text-white text-center text-2xl mb-3">Sign In</h1>
           <h3 className="text-white text-center text-sm mb-10">Unite with style, sell with confidence</h3>
           <form onSubmit={handleSubmit}>
             <input
               type="text"
-              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage ? "border-red-500" : ""}`}
+              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage.email ? "border-red-500" : ""}`}
               placeholder="Email"
               onChange={(e) => setEmail(e.target.value)}
               name="email"
               value={email}
             />
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {errorMessage.email && <p className="text-red-500">{errorMessage.email}</p>}
             <input
               type="password"
               name="password"
               id="password"
-              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage ? "border-red-500" : ""}`}
+              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage.password ? "border-red-500" : ""}`}
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              style={{ borderColor: errorMessage.password ? "red" : "" }}
             />
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {errorMessage.password && <p className="text-red-500">{errorMessage.password}</p>}
             <button type="submit" className="w-full p-2 border rounded-full bg-blue-800 mt-10 text-white hover:bg-blue-700">
               LOGIN
             </button>
