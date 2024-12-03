d44fe84d3068a23cf908324405802305d00b3db4
adhlira <adhliramadhan68@gmail.com>
2024-12-03 12:45:40 +0700

    add user logic

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/rules-of-hooks */
-import { data, Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
@@ -8,73 +7,82 @@ const login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState({ email: "", password: "" });
 
   const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const errors = {};
+    try {
+      const response = await axios.post("http://localhost:3000/login", {
+        email,
+        password,
+      });
+      console.log("Ini adalah response", response);
 
-    if (!email) {
-      errors.email = "Email tidak boleh kosong";
-    }
-    if (!password) {
-      errors.password = "Password tidak boleh kosong";
-    }
-    setErrorMessage(errors);
-
-    if (Object.keys(errors).length === 0)
-      try {
-        const response = await axios.post("http://localhost:3000/login", {
-          email,
-          password,
-        });
-        console.log("Ini adalah response", response);
-
-        if (response.status === 200) {
-          // Simpan data user di localStorage
-          localStorage.setItem("user", JSON.stringify(response.data.user));
-          alert("Succesfully Login!");
-          navigate("/index-seller");
-        }
-      } catch (error) {
-        console.log("ini adalah error", error);
-        if (error.response && error.response.status === 400) {
-          alert("Email atau password salah");
-          setEmail("");
-          setPassword("");
-        }
+      if (response.status === 200) {
+        // Simpan data user di localStorage
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        alert("Succesfully Login!");
+        navigate("/index-seller");
       }
+    } catch (error) {
+      console.log("ini adalah error", error);
+      if (error.response && error.response.status === 400) {
+        alert("Email atau password salah");
+        setEmail("");
+        setPassword("");
+      }
+    }
   };
 
   return (
     <>
       <div className="bg-blue-800 flex justify-center h-screen">
-        <div className="container mx-auto place-self-center rounded-md p-5 w-80 h-[420px] bg-gray-800 font-serif">
+        <div className="container mx-auto place-self-center rounded-md p-5 w-80 h-[450px] bg-gray-800 font-serif">
           <h1 className="text-white text-center text-2xl mb-3">Sign In</h1>
           <h3 className="text-white text-center text-sm mb-10">Unite with style, sell with confidence</h3>
           <form onSubmit={handleSubmit}>
             <input
               type="text"
-              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage.email ? "border-red-500" : ""}`}
+              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline`}
               placeholder="Email"
               onChange={(e) => setEmail(e.target.value)}
               name="email"
               value={email}
+              required
             />
-            {errorMessage.email && <p className="text-red-500">{errorMessage.email}</p>}
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline ${errorMessage.password ? "border-red-500" : ""}`}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errorMessage.password && <p className="text-red-500">{errorMessage.password}</p>}
+            <div className="mb-4 mt-5">
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  className={`shadow appearance-none w-full rounded-lg border p-2 mt-5 leading-tight focus:outline-none focus:shadow-outline`}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <button type="button" onClick={togglePasswordVisibility} className="absolute inset-y-0 right-0 flex items-center px-2 mt-4 text-gray-500 focus:outline-none">
+                  {showPassword ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M2.166 10.11C3.83 6.55 6.97 4 10 4c3.03 0 6.17 2.55 7.833 6.11a1.5 1.5 0 010 1.78C16.17 13.45 13.03 16 10 16c-3.03 0-6.17-2.55-7.833-6.11a1.5 1.5 0 010-1.78zM10 6a6.002 6.002 0 00-5.884 5.26C5.335 11.422 7.49 13 10 13c2.51 0 4.665-1.578 5.884-3.74A6.002 6.002 0 0010 6zm0 2a2 2 0 110 4 2 2 0 010-4z" />
+                      <path d="M10 8a2 2 0 100 4 2 2 0 000-4zm0 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M10 3a7 7 0 100 14 7 7 0 000-14zM10 1a9 9 0 110 18A9 9 0 0110 1z" />
+                      <path d="M14.828 5.172a4 4 0 010 5.656 1 1 0 01-1.414-1.414 2 2 0 000-2.828 1 1 0 111.414-1.414zM5.172 5.172a4 4 0 015.656 0 1 1 0 11-1.414 1.414 2 2 0 00-2.828 0 1 1 0 11-1.414-1.414z" />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
             <button type="submit" className="w-full p-2 border rounded-full bg-blue-800 mt-10 text-white hover:bg-blue-700">
               LOGIN
             </button>
--- src/pages/sign_up.jsx
+++ src/pages/sign_up.jsx
@@ -1,47 +1,94 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
-function sign_up() {
+const sign_up = () => {
+  const [data, setData] = useState({ name: "", email: "", password: "", address: "", phone_number: "", role_id: "" });
+
+  const navigate = useNavigate();
+
+  const handleCheckbox = (event) => {
+    const value = parseInt(event.target.value);
+    if (event.target.checked) {
+      setUserType((prev) => [...prev, value]);
+    } else {
+      setUserType((prev) => prev.filter((role) => role !== value));
+    }
+  };
+
+  const handleChange = (e) => {
+    const [name, value] = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      address: data.address,
+      phone_number: data.phone_number,
+      role_id: data.role_id,
+    };
+    console.log("ini adalah user data", userData);
+
+    try {
+      const response = await axios.post("http://localhost:3000/sign-up", userData);
+      console.log("ini adalah response", response);
+
+      if (response.status === 200) {
+        alert("Sign Up User successfully");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("Terjadi kesalahan", error);
+    }
+  };
+
   return (
     <>
       <div className="bg-blue-800 flex justify-center h-screen">
         <div className="container mx-auto p-5 border rounded-md bg-gray-800 w-[800px] font-serif mb-1 mt-1">
           <h1 className="text-white text-center text-2xl mb-10">Sign Up</h1>
-          <form action="" className="">
+          <form onSubmit={handleSubmit}>
             <div className="flex gap-4">
               <div className="flex-1">
                 <label htmlFor="name" className="text-white">
                   Name
                 </label>
-                <input type="text" className="w-full rounded-lg border p-2 mt-2 mb-5" name="name" placeholder="Your name" />
+                <input type="text" className="w-full rounded-lg border p-2 mt-2 mb-5" name="name" placeholder="Your name" onChange={handleChange} value={data.name} required />
 
                 <label htmlFor="email" className="text-white">
                   Email
                 </label>
-                <input type="text" name="email" id="email" className="w-full rounded-lg border p-2 mt-2 mb-5" placeholder="Your Email" />
+                <input type="text" name="email" id="email" className="w-full rounded-lg border p-2 mt-2 mb-5" placeholder="Your Email" onChange={handleChange} value={data.email} required />
 
                 <label htmlFor="address" className="text-white">
                   Address
                 </label>
-                <textarea name="address" id="address" className="w-full rounded-lg mt-2 h-36" placeholder=" Your address"></textarea>
+                <textarea name="address" id="address" className="w-full rounded-lg mt-2 h-36" placeholder=" Your address" onChange={handleChange} value={data.address} required></textarea>
               </div>
               <div className="flex-1">
                 <label htmlFor="phone_number" className="text-white">
                   Phone Number
                 </label>
-                <input type="text" name="phone_number" id="phone_number" className="w-full p-2 mt-2 mb-5 rounded-lg" placeholder="Your phone number" />
+                <input type="text" name="phone_number" id="phone_number" className="w-full p-2 mt-2 mb-5 rounded-lg" placeholder="Your phone number" onChange={handleChange} value={data.phone_number} required />
                 <label htmlFor="password" className="text-white">
                   Password
                 </label>
-                <input type="password" name="password" id="password" className="w-full p-2 mt-2 mb-5 rounded-lg" placeholder="Your password" />
+                <input type="password" name="password" id="password" className="w-full p-2 mt-2 mb-5 rounded-lg" placeholder="Your password" onChange={handleChange} value={data.password} required />
                 <label htmlFor="user_type" className="text-white">
                   User Type
                 </label>
                 <br />
                 <div className="flex items-center space-x-2 mt-2">
-                  <input type="checkbox" name="seller" id="seller" className="text-white" />
+                  <input type="checkbox" name="role_id" id="seller" value="1" className="text-white" onChange={handleCheckbox} />
                   <p className="text-white">Seller</p>
 
-                  <input type="checkbox" name="buyer" id="buyer" className="text-white" />
+                  <input type="checkbox" name="role_id" id="buyer" value="2" className="text-white" onChange={handleCheckbox} />
                   <p className="text-white">Buyer</p>
                 </div>
               </div>
@@ -66,5 +113,5 @@ function sign_up() {
       </div>
     </>
   );
-}
+};
 export default sign_up;
